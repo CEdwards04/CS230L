3cde161aa7fee185278568ceebcdfcf916373704
CEdwards04 <cmedwards2004@gmail.com>
2024-02-25 14:32:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
        <h1>CS 230L</h1>
-      <h2>Section - XX</h2>
-      <p>WVU ID: XXXXXXX</p>
+      <h2>Section - 001</h2>
+      <p>WVU ID: 800389448</p>
       <p>Hi, I am Caleb</p>
       </div>
   );
