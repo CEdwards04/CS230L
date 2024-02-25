8c0236d7992e407d890d3239a4b9cefcc74f17d2
CEdwards04 <cmedwards2004@gmail.com>
2024-02-25 14:18:47 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>CS 230L</h1>
+      <h2>Section - XX</h2>
+      <p>WVU ID: XXXXXXX</p>
+      <p>Hi, I am Caleb</p>
+      </div>
+  );
+}
+
+export default App;
